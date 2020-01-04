d23c8b42cbf5d5d420d96c811f0392cf7d9ecf3a
hidehiko yanagida <hyanagida.0721@gmail.com>
2020-01-04 20:22:41 +0900

    firebaseの導入、設定など

--- chat/src/ChatApp.tsx
+++ chat/src/ChatApp.tsx
@@ -5,6 +5,9 @@ import ChangeNameFormDialog from "./components/ChangeNameFormDialog";
 import CreateRoomFormDialog from "./components/CreateRoomFormDialog";
 import StickyFooter from "./components/StickyFooter";
 
+// import "firebase/firestore";
+// import db from "./firebaseInit";
+
 export default function ChatApp() {
   return (
     <div>
@@ -15,3 +18,35 @@ export default function ChatApp() {
     </div>
   );
 }
+
+// firebase.initializeApp(config);
+// const db = firebase.firestore();
+
+// db.collection("users")
+//   .add({
+//     first: "Adaaaaaaa",
+//     last: "Lovelacesdfajflsaj",
+//     born: 1815
+//   })
+//   .then(function(docRef) {
+//     console.log("Document written with ID: ", docRef);
+//   })
+//   .catch(function(error) {
+//     console.error("Error adding document: ", error);
+//   });
+
+// db.collection("users")
+//   .get()
+//   .then(querySnapshot => {
+//     querySnapshot.forEach(doc => {
+//       console.log(`${doc.id} => ${doc.data()}`);
+//     });
+//   });
+// db.collection("users").onSnapshot(querySnapShot => {
+//   querySnapShot.forEach(doc => {
+//     console.log(doc.data());
+//     console.log(
+//       doc.data().first + ":" + doc.data().last + ":" + doc.data().born
+//     );
+//   });
+// });
